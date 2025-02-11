16e848a79b983dcbf599aa019df7cddb53f4ec40
PRAMODSB <sbpramod4@gmail.com>
2025-02-11 15:21:20 +0530

    deleted comments

--- FreeRTOS_TM4C129ENCPDT-master/FreeRTOS_TM4C129ENCPDT-master/tm4c129encpdt_startup_ccs.c
+++ FreeRTOS_TM4C129ENCPDT-master/FreeRTOS_TM4C129ENCPDT-master/tm4c129encpdt_startup_ccs.c
@@ -1,25 +1,4 @@
-//*****************************************************************************
-//
-// Startup code for use with TI's Code Composer Studio.
-//
-// Copyright (c) 2011-2014 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-// Software License Agreement
-//
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-//
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
 
-//
-//*****************************************************************************
 
 #include <stdint.h>
 #include "FreeRTOS.h"
